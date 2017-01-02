2fd462f4e22054ae94a8347452c93a8f9626aa63
David Wang <david.wang34@gmail.com>
2017-01-01 22:05:46 -0500

    design changes

--- DontSpend/AboutViewController.swift
+++ DontSpend/AboutViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class AboutViewController: UIViewController, UITextFieldDelegate {
+    @IBAction func dollarValueChanged(_ sender: Any) {
+    }
     
     @IBAction func changeValueButton(_ sender: Any) {
         
@@ -17,20 +19,16 @@ class AboutViewController: UIViewController, UITextFieldDelegate {
         let longTermRate = Double(longTermRateField.text!)
         let SP500Rate = Double(SP500RateField.text!)
         
-        if let i = inflationRate, i < 100
+        if let i = inflationRate, i < 100, let j = shortTermRate, j < 100, let l = longTermRate, l < 100, let m = SP500Rate, m < 100 {
+            
+            print("Good value")
+            
+            return
+        }
         
-        /*
-         
-         
-         
-         if let v = amountDollars, v > 100000.00 {
-         
-         present(alertController, animated: true, completion: nil)
-         
-         return
-         }*/
-        print("change button pressed")
     }
+    
+    //can remove this later
     @IBOutlet weak var dollarValue: UISegmentedControl!
     
     @IBOutlet weak var inflationRateField: UITextField!
@@ -40,7 +38,12 @@ class AboutViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBAction func defaultButton(_ sender: Any) {
-        print("Default button pressed")
+        
+        inflationRateField.text = "3.07"
+        shortTermRateField.text = "3.49"
+        longTermRateField.text = "5.00"
+        SP500RateField.text = "9.60"
+        
     }
 
     @IBAction func linkButton(_ sender: Any) {
@@ -52,6 +55,7 @@ class AboutViewController: UIViewController, UITextFieldDelegate {
         
         self.view.backgroundColor = UIColor(red: 93/255.0, green: 100/255.0, blue: 118/255.0, alpha: 1.0)
         
+        //only allows one decimal place
         inflationRateField.delegate = self
         shortTermRateField.delegate = self
         longTermRateField.delegate = self
@@ -72,6 +76,7 @@ class AboutViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    //only allows one decimal place
     func textField(_ textField: UITextField,shouldChangeCharactersIn range: NSRange,replacementString string: String) -> Bool
     {
         let countdots = (textField.text?.components(separatedBy: ".").count)! - 1
