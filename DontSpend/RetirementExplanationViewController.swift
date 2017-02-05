d22f1ad2e2c61fac8207a714404b2f6083222c1a
David Wang <david.wang34@gmail.com>
2017-02-04 22:07:08 -0500

    functionality additions

---
+++ DontSpend/RetirementExplanationViewController.swift
@@ -0,0 +1,72 @@
+//
+//  RetirementExplanationViewController.swift
+//  DontSpend
+//
+//  Created by David Wang on 2/4/17.
+//  Copyright © 2017 David Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RetirementExplanationViewController: UIViewController {
+    
+    @IBOutlet weak var monthlyExpenses: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    let alertController = UIAlertController(title: "Error", message: "Please fill the expenses field with a vaue less than 100,000", preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //dismisses keyboard on tap
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: #selector(ViewController.didTapView))
+        self.view.addGestureRecognizer(tapRecognizer)
+        
+        alertController.addAction(defaultAction)
+        
+        self.view.backgroundColor = UIColor(red: 93/255.0, green: 100/255.0, blue: 118/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        self.resultLabel.text = ""
+
+        
+    }
+    
+    @IBAction func calculateButton(sender: AnyObject)
+    {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency
+        currencyFormatter.locale = NSLocale.current
+        
+        let monthlyExpensesDouble = Double(monthlyExpenses.text!)
+        
+        if let i = monthlyExpensesDouble, i < 100000 {
+            let retirementAmount = monthlyExpensesDouble! * 25
+            
+            self.resultLabel.text = currencyFormatter.string(from: NSNumber(value:retirementAmount))!
+            
+            return
+        }
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    
+    func didTapView(){
+        self.view.endEditing(true)
+    }
+
+
+    @IBAction func cancelScreen(sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- DontSpend/RetirementViewController.swift
+++ DontSpend/RetirementViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class RetirementViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+class RetirementViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var currentNWInput: UITextField!
     @IBOutlet weak var amtPerYear: UITextField!
@@ -25,8 +25,6 @@ class RetirementViewController: UIViewController, UITextFieldDelegate, UIPickerV
     @IBOutlet weak var categoryPicker: UIPickerView!
     
     @IBOutlet weak var frequencyControl: UISegmentedControl!
-    
-    var categoryPickerData = ["Short Term (3 Month T. Bill)", "Long Term (10 Year T. Bond)", "S&P 500"]
     
     var inflation: Double = 0.0329
     var returnForSP500: Double = 0.0950
@@ -71,73 +69,32 @@ class RetirementViewController: UIViewController, UITextFieldDelegate, UIPickerV
         returnForLongTerm = secondTab.longTermRate
         returnForSP500 = secondTab.SP500Rate
         
-        let selection = categoryPicker.selectedRow(inComponent: 0)
-        
         let currencyFormatter = NumberFormatter()
         currencyFormatter.usesGroupingSeparator = true
         currencyFormatter.numberStyle = NumberFormatter.Style.currency
         currencyFormatter.locale = NSLocale.current
         
         if let i = currentNWDouble, i < 9000000, let j = amtPerYearDouble, j < 9000000, let l = targetRetirementAmtDouble, l < 9000000, let m = spendAmountDouble, m < 9000000, i < l {
-            
-            
-            if selection == 0 {
-                let yearsUntilRetirementDouble: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForShortTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
+ 
+                let yearsUntilRetirementDouble: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForSP500 - inflation, targetWorth: targetRetirementAmtDouble!, payment: 12.0*amtPerYearDouble!)
                 
                 yearsUntilRetirement.text = String(format: "%.2f", yearsUntilRetirementDouble)
                 
                 if onceOrMonth == 0 { //once
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble! + spendAmountDouble!, rate: returnForShortTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
+                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble! + spendAmountDouble!, rate: returnForShortTerm, targetWorth: targetRetirementAmtDouble!, payment: 12.0*amtPerYearDouble!)
                     
                     yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
     
                 }
             
                 if onceOrMonth == 1 { //monthly
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForShortTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble! + spendAmountDouble!)
+                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForShortTerm, targetWorth: targetRetirementAmtDouble!, payment: 12.0*(amtPerYearDouble! + spendAmountDouble!))
         
                     yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
                 }
                 
                 
-            } else if selection == 1 {
-                let yearsUntilRetirementDouble: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForLongTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
-                
-                yearsUntilRetirement.text = String(format: "%.2f", yearsUntilRetirementDouble)
-                
-                if onceOrMonth == 0 { //once
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble! + spendAmountDouble!, rate: returnForLongTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
-                    
-                    yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
-                    
-                }
-                
-                if onceOrMonth == 1 { //monthly
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForLongTerm, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble! + spendAmountDouble!)
-                    
-                    yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
-                }
-                
-            } else if selection == 2 {
-                let yearsUntilRetirementDouble: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForSP500, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
-                
-                yearsUntilRetirement.text = String(format: "%.2f", yearsUntilRetirementDouble)
-                
-                if onceOrMonth == 0 { //once
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble! + spendAmountDouble!, rate: returnForSP500, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble!)
-                    
-                    yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
-                    
-                }
-                
-                if onceOrMonth == 1 { //monthly
-                    let newYearsUntilRetirement: Double = yearsUntilRetirementFunc(netWorth: currentNWDouble!, rate: returnForSP500, targetWorth: targetRetirementAmtDouble!, payment: amtPerYearDouble! + spendAmountDouble!)
-                    
-                    yearDifference = yearsUntilRetirementDouble - newYearsUntilRetirement
-                    
-                }
-            }
-            
+        
             summaryField.text = "If you saved $" + spendAmount.text! + " instead of spending it, you would be able to retire " + String(format: "%.2f", yearDifference) + " years, or " + String(format: "%.2f", yearDifference * 365) + " days earlier!"
             
             return
@@ -150,9 +107,6 @@ class RetirementViewController: UIViewController, UITextFieldDelegate, UIPickerV
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        categoryPicker.dataSource = self
-        categoryPicker.delegate = self
-
         let tapRecognizer = UITapGestureRecognizer()
         tapRecognizer.addTarget(self, action: #selector(ViewController.didTapView))
         self.view.addGestureRecognizer(tapRecognizer)
@@ -189,35 +143,10 @@ class RetirementViewController: UIViewController, UITextFieldDelegate, UIPickerV
     func returnsIfInvested(amount: Double, rate: Double, years: Double) -> NSNumber {
             return NSNumber(value: amount*pow(1 + rate - inflation,years))
     }
-    
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int
-    {
-        return 1
-    }
-    
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
-    {
-        return categoryPickerData.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return categoryPickerData[row]
-    }
+
     
     func didTapView(){
         self.view.endEditing(true)
-    }
-
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        // LOOK AGAIN AT THIS CODE
-        let pickerLabel = UILabel()
-        let titleData = categoryPickerData[row]
-        let myTitle = NSAttributedString(string: titleData, attributes: [NSForegroundColorAttributeName:UIColor(red: 207.0/255.0, green: 211.0/255.0, blue: 212.0/255.0, alpha: 1)])
-        
-        pickerLabel.attributedText = myTitle
-        pickerLabel.textAlignment = .center
-        //pickerLabel.backgroundColor = UIColor(hue: 1, saturation: 1.0, brightness:1.0, alpha: 1.0)
-        return pickerLabel
     }
 
 
